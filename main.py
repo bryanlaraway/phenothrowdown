2fee341a1d42561e04c0d00cbf05fda370b5160c
Bryan Laraway <larawayb@ohsu.edu>
2015-04-04 15:01:09 -0700

    Add main.py.

---
+++ main.py
@@ -0,0 +1,3 @@
+#Author: Bryan Laraway
+#Project: Comparison of OWLSim and Phenologs for the identification of models of human disease and gene candidates for human disease.
+#Purpose: This script will call of the functions/methods/scripts for performing the processing required for this analysis.
