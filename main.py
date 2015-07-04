0d11fd25eaade166bf72941d60abaa46aecd5b3c
Bryan Laraway <larawayb@ohsu.edu>
2015-07-04 14:11:10 -0700

    Running adjustments.

--- main.py
+++ main.py
@@ -3483,11 +3483,11 @@ fdr_cutoff = 0.004426898733810069
 
 
 #main.set_stage_for_extension_fdr_calculation()
-
+#gc.set_debug(gc.DEBUG_LEAK)
 with open('inter/phenolog/hvz_phenolog_combo.txt', 'rb') as handle:
     read_only_hvz_phenologs = set(pickle.load(handle))
 
-for i in range(330, 340):
+for i in range(340, 350):
     with open('inter/random/human/random_ext_'+str(i)+'.txt', 'rb') as handle:
         read_only_human_geno_pheno_hash = pickle.load(handle)
     with open('inter/random/zebrafish/random_ext_'+str(i)+'.txt', 'rb') as handle:
