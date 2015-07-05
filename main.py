96a76d18de943185148bdf8db66de212ae03b08d
bryanlaraway <bryanlaraway@gmail.com>
2015-07-04 23:44:14 -0700

    Adjustment of hvz phenolog extension FDR calculation set.

--- main.py
+++ main.py
@@ -1013,7 +1013,7 @@ class main():
             #filereader = csv.reader(csvfile, delimiter='\t', quotechar='\"')
             #row_count = sum(1 for row in filereader)
             #print(str(row_count)+' rows to process.')
-        for x in range(1, 2): #num_files+1
+        for x in range(2, num_files+1): #num_files+1
             interfile = interfile_directory+'/'+interfile_prefix+'_'+str(x)+'.txt'
             outfile = outfile_directory+'/'+outfile_prefix+'_'+str(x)+'.txt'
 
@@ -3384,7 +3384,7 @@ main = main()
 #main.perform_owlsim_queries('inter/hpo/human_disease_phenotype_hash.txt', 'inter/mgi/mouse_genotype_phenotype_hash.txt', 'inter/owlsim/human_disease_mouse_genotype', 'human_disease_mouse_genotype_queries', 'out/owlsim/human_disease_mouse_genotype', 'human_disease_mouse_genotype_results', 104)
 #print('INFO: Done processing human diseases vs mouse genotypes')
 
-#Processing completed in  hours,  comparisons.
+#Process completed!
 #Human Diseases = 9214
 #zebrafish Genotype = 8535
 #Total comparisons = 78,641,490
@@ -3492,7 +3492,7 @@ fdr_cutoff = 0.004426898733810069
 with open('inter/phenolog/hvz_phenolog_combo.txt', 'rb') as handle:
     read_only_hvz_phenologs = set(pickle.load(handle))
 
-for i in range(380, 390):
+for i in range(390, 400):
     with open('inter/random/human/random_ext_'+str(i)+'.txt', 'rb') as handle:
         read_only_human_geno_pheno_hash = pickle.load(handle)
     with open('inter/random/zebrafish/random_ext_'+str(i)+'.txt', 'rb') as handle:
