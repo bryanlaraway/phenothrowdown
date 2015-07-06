a0ee2973e3991738150f2602711220c449825f15
bryanlaraway <bryanlaraway@gmail.com>
2015-07-06 13:40:35 -0700

    Running adjustments, removal of counter.

--- main.py
+++ main.py
@@ -3438,7 +3438,7 @@ def multiprocess_ext_fdr_calculation_hvz(comparison_list):
         return
 
 def multiprocess_ext_fdr_calculation_hvm(comparison_list):
-    increment()
+    #increment()
 
     total_phenotype_matches = 0
     total_phenotype_nonmatches = 0
@@ -3498,7 +3498,7 @@ def multiprocess_ext_fdr_calculation_hvm(comparison_list):
         return
 
 def multiprocess_ext_fdr_calculation_mvz(comparison_list):
-    increment()
+    #increment()
 
     total_phenotype_matches = 0
     total_phenotype_nonmatches = 0
@@ -3749,7 +3749,7 @@ for i in range(500, 1001):
 '''
 with open('inter/phenolog/hvm_phenolog_combo.txt', 'rb') as handle:
     read_only_hvm_phenologs = set(pickle.load(handle))
-for i in range(1, 2):
+for i in range(1, 1001):
     with open('inter/random/human/random_ext_'+str(i)+'.txt', 'rb') as handle:
         read_only_human_geno_pheno_hash = pickle.load(handle)
     with open('inter/random/mouse/random_ext_'+str(i)+'.txt', 'rb') as handle:
@@ -3767,7 +3767,7 @@ for i in range(1, 2):
 
 with open('inter/phenolog/mvz_phenolog_combo.txt', 'rb') as handle:
     read_only_mvz_phenologs = set(pickle.load(handle))
-for i in range(1, 2):
+for i in range(1, 1001):
     with open('inter/random/mouse/random_ext_'+str(i)+'.txt', 'rb') as handle:
         read_only_mouse_geno_pheno_hash = pickle.load(handle)
     with open('inter/random/zebrafish/random_ext_'+str(i)+'.txt', 'rb') as handle:
