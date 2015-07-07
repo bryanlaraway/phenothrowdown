9f56ec8f2e278265eb4b1e3e95d0cbb2bef9b4f1
bryanlaraway <bryanlaraway@gmail.com>
2015-07-06 20:27:39 -0700

    Running adjustments. Deletion of lists.

--- main.py
+++ main.py
@@ -3732,7 +3732,7 @@ fdr_cutoff = 0.004426898733810069
 
 with open('inter/phenolog/hvz_phenolog_combo.txt', 'rb') as handle:
     read_only_hvz_phenologs = set(pickle.load(handle))
-for i in range(500, 1001):
+for i in range(512, 1001):
     with open('inter/random/human/random_ext_'+str(i)+'.txt', 'rb') as handle:
         read_only_human_geno_pheno_hash = pickle.load(handle)
     with open('inter/random/zebrafish/random_ext_'+str(i)+'.txt', 'rb') as handle:
